e772ef3555a52aee410558e431364df70e901a5d
Dennis Ronny <dennisor98@gmail.com>
2024-11-14 13:37:31 +0300

    handled test account exception

--- src/main/java/net/sasakonnect/wifi_portal/services/PaymentService.java
+++ src/main/java/net/sasakonnect/wifi_portal/services/PaymentService.java
@@ -19,9 +19,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.gson.Gson;
