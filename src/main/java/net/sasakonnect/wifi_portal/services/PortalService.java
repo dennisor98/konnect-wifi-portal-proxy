fb258417cc3e9423722d1c5f9a555e5119acd03c
Dennis Ronny <dennisor98@gmail.com>
2024-11-15 21:46:49 +0300

    added tv connection

--- src/main/java/net/sasakonnect/wifi_portal/services/PortalService.java
+++ src/main/java/net/sasakonnect/wifi_portal/services/PortalService.java
@@ -1,5 +1,7 @@
 package net.sasakonnect.wifi_portal.services;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.sasakonnect.wifi_portal.RequestDto.AddDeviceDto;
 import net.sasakonnect.wifi_portal.RequestDto.ChangeDeviceDto;
 import net.sasakonnect.wifi_portal.RequestDto.ConnectTvDto;
+import net.sasakonnect.wifi_portal.RequestDto.KompVlanDto;
 import net.sasakonnect.wifi_portal.RequestDto.KompVlanDto;
 import net.sasakonnect.wifi_portal.RequestDto.PackageByMacDto;
 import net.sasakonnect.wifi_portal.RequestDto.PollMpesaDto;
@@ -489,9 +492,8 @@ public class PortalService {
            String requestBody = "pagetype="+pageType+"&vlan="+tvconnect.getVlan()+"&staMac="+tvconnect.getStaMac();
            log.error(requestBody+"{req}");
 		   Mono<String> responseMono =  this.defaultWeclientBean.webClient.post().uri(PortalEndpointsConstant.WEB_PORTAL_AUTH)
-				   
 				   .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-				   .bodyValue(requestBody)
+				   .bodyValue(this.createUrlEncodedRequestBody(tvconnect))
 				   .header("Authorization","Basic JDJhJDEwJExhQWg1eGhjbzpaMGhLSng1UnZ5bGVHNEhwdkQ3")
 				   .accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(String.class);
 		   
@@ -524,6 +526,8 @@ public class PortalService {
 			   .toUriString();
 	   Mono<String> response =  this.defaultWeclientBean.webClient.get().uri(uri)
 			   .header("Authorization","Bearer "+getKompAuthToken())
+	   Mono<String> response =  this.defaultWeclientBean.webClient.get().uri(uri)
+			   .header("Authorization","Bearer "+getKompAuthToken())
 			   .accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(String.class);
 
 	   
@@ -535,18 +539,11 @@ public class PortalService {
 			var build = ConnectTvDto.builder().mode(resp.getData().getModel().toLowerCase())
 					.publicIp(resp.getData().getPublicIp()).staIp(tvconnect.getStaIp())
 					.staMac(tvconnect.getStaMac()).vlan(resp.getData().getVlan().getVlanName().replace("v","")).build();
-//			log.error("body{}"+body);
-			if(resp.getData().getModel().equalsIgnoreCase("gpon")) {
-				   pageType = "remote";
-			   }else {
-				   pageType = "100";
-			   }
-		   
-			String requestBody = "pagetype="+pageType+"&vlan="+resp.getData().getVlan().getVlanName().replace("v","") +"&staMac="+tvconnect.getStaMac();
-			log.error(requestBody+"{req}");
+			var body = this.createUrlEncodedRequestBody(build);
+			log.error("body{}"+body);
 			Mono<String> responseMono =  this.defaultWeclientBean.webClient.post().uri(PortalEndpointsConstant.WEB_PORTAL_AUTH)
-					.contentType(MediaType.APPLICATION_FORM_URLENCODED) // Set content type to form-urlencoded
-					.bodyValue(requestBody)
+					   .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+					   .bodyValue(body)
 					   .header("Authorization","Basic JDJhJDEwJExhQWg1eGhjbzpaMGhLSng1UnZ5bGVHNEhwdkQ3")
 					   .accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(String.class);
 			
@@ -563,10 +560,10 @@ public class PortalService {
 				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(node);
 			}
 			
-		   }
+			
 		
 			
-		   
+		   }
 	   }catch(Exception ex) {
 		   ex.printStackTrace();
 		   ObjectNode node = JsonNodeFactory.instance.objectNode();
