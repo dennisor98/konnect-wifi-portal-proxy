ac076d3554327c4f14e21a0396051b3e68b13b31
Dennis Ronny <dennisor98@gmail.com>
2024-11-20 17:03:18 +0300

    fixed rabitMq bug

--- src/main/java/net/sasakonnect/wifi_portal/config/RabitMqConfig.java
+++ src/main/java/net/sasakonnect/wifi_portal/config/RabitMqConfig.java
@@ -1,44 +1,44 @@
-package net.sasakonnect.wifi_portal.config;
-
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-
-import com.rabbitmq.client.Channel;
-
-@Component
-public class RabitMqConfig {
-		private static final String QUEUE_NAME = "merchant";
-		private static final String CHANNEL_NAME = "merchant";
-
-
-	    // Define the Connection Factory
-	    @Bean
-	     ConnectionFactory connectionFactory() {
-	        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
-	        connectionFactory.setUsername("guest"); // Default username
-	        connectionFactory.setPassword("guest"); // Default password
-	        return connectionFactory;
-	    }
-
-	    // Define a RabbitTemplate Bean
-	    @Bean
-	     RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-	        return new RabbitTemplate(connectionFactory);
-	    }
-
-	    // Define a Queue
-	    @Bean
-	     Queue queue() {
-	        return new Queue(QUEUE_NAME, false); // Durable = false
-	    }
-	    
+//package net.sasakonnect.wifi_portal.config;
+//
+//import org.springframework.amqp.core.Queue;
+//import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+//import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Component;
+//
+//import com.rabbitmq.client.Channel;
+//
+//@Component
+//public class RabitMqConfig {
+//		private static final String QUEUE_NAME = "merchant";
+//		private static final String CHANNEL_NAME = "merchant";
+//
+//
+//	    // Define the Connection Factory
 //	    @Bean
-//	     Channel channel() {
-//	        return Channel
+//	     ConnectionFactory connectionFactory() {
+//	        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+//	        connectionFactory.setUsername("guest"); // Default username
+//	        connectionFactory.setPassword("guest"); // Default password
+//	        return connectionFactory;
 //	    }
-	
-}
+//
+//	    // Define a RabbitTemplate Bean
+//	    @Bean
+//	     RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+//	        return new RabbitTemplate(connectionFactory);
+//	    }
+//
+//	    // Define a Queue
+//	    @Bean
+//	     Queue queue() {
+//	        return new Queue(QUEUE_NAME, false); // Durable = false
+//	    }
+//	    
+////	    @Bean
+////	     Channel channel() {
+////	        return Channel
+////	    }
+//	
+//}
