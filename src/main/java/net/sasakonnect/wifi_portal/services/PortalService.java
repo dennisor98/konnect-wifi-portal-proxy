d508bb098caec657265a25b16cfe09f4c211c61d
Dennis Ronny <dennisor98@gmail.com>
2024-11-20 16:48:27 +0300

    changed chat base endpoint

--- src/main/java/net/sasakonnect/wifi_portal/services/PortalService.java
+++ src/main/java/net/sasakonnect/wifi_portal/services/PortalService.java
@@ -1,7 +1,5 @@
 package net.sasakonnect.wifi_portal.services;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
