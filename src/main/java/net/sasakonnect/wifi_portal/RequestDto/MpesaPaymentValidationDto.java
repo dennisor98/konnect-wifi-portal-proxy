1a787b713d96b379fa8a7ecfd0bc710da280e972
Dennis Ronny <dennisor98@gmail.com>
2024-12-04 14:51:39 +0300

    rectifified mpesa validation dto bug

--- src/main/java/net/sasakonnect/wifi_portal/RequestDto/MpesaPaymentValidationDto.java
+++ src/main/java/net/sasakonnect/wifi_portal/RequestDto/MpesaPaymentValidationDto.java
@@ -1,19 +1,32 @@
 package net.sasakonnect.wifi_portal.RequestDto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class MpesaPaymentValidationDto {
+  @JsonProperty("TransactionType")
   String TransactionType;
+  @JsonProperty("TransID")
   String TransID;
+  @JsonProperty("TransTime")
   String TransTime;
+  @JsonProperty("TransAmount")
   String TransAmount;
+  @JsonProperty("BusinessShortCode")
   String BusinessShortCode;
+  @JsonProperty("BillRefNumber")
   String BillRefNumber;
+  @JsonProperty("InvoiceNumber")
   String InvoiceNumber;
+  @JsonProperty("OrgAccountBalance")
   String OrgAccountBalance;
+  @JsonProperty("ThirdPartyTransID")
   String ThirdPartyTransID;
+  @JsonProperty("MSISDN")
   String MSISDN;
+  @JsonProperty("FirstName")
   String FirstName;
   
 }
