763a8d6c3195b4d0adf63552eb7834b20f9e2b0c
Dennis Ronny <dennisor98@gmail.com>
2024-12-05 11:39:16 +0300

    modified mpesa transaction status fetch api

--- src/main/java/net/sasakonnect/wifi_portal/constants/MpesaEndpointsConstants.java
+++ src/main/java/net/sasakonnect/wifi_portal/constants/MpesaEndpointsConstants.java
@@ -3,6 +3,6 @@ package net.sasakonnect.wifi_portal.constants;
 public class MpesaEndpointsConstants {
 	public final  static  String STK_PUSH="/mpesa/stkpush/v1/processrequest";
 	public final  static  String TX_QUERY="/mpesa/stkpushquery/v1/query";
-	public final  static  String TX_STATUS="/transactionstatus/v1/query";   
+	public final  static  String TX_STATUS="/mpesa/transactionstatus/v1/query";   
 	
 }
