1002373414e1adcf9f377c67742386c47bed91a2
Dennis Ronny <dennisor98@gmail.com>
2024-12-10 11:58:43 +0300

    modified app payment Request Dto and added fields to notification callBack

---
+++ src/main/java/net/sasakonnect/wifi_portal/RequestDto/AppToolKitPayDto.java
@@ -0,0 +1,12 @@
+package net.sasakonnect.wifi_portal.RequestDto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class AppToolKitPayDto {
+	@NotNull()
+	String mobileNumber;
+	
+	String staMac;
+}
