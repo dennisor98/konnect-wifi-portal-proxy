d98a8cc4432deee56c081dff09d367e4fecce5a0
Dennis Ronny <dennisor98@gmail.com>
2024-12-11 14:31:02 +0300

    added default app for payment as wifi

--- src/main/java/net/sasakonnect/wifi_portal/services/PaymentService.java
+++ src/main/java/net/sasakonnect/wifi_portal/services/PaymentService.java
@@ -81,6 +81,9 @@ public class PaymentService {
 	//	
 	@Value("${mpesaCallBackUrl}")
 	String mpesaCallBackUrl;
+	
+	@Value("${wifi.app.key}")
+	String wifiAppKey;
 	//	
 	@Autowired
 	AuthService authService;
@@ -710,8 +713,9 @@ public class PaymentService {
 
 	public Object createPaymentRequest(AppToolKitPayDto req) {
 		App app = null;
-		var appKey = "f7bc83f430538424b13298e6aa6fb143efd8427454f7f9a3e49e91d90c416b0e";
-		Optional<App> appOpt =  this.appRepository.findFirstByAppKeyAndAppSecret(appKey);
+		
+//		var appKey = "f7bc83f430538424b13298e6aa6fb143efd8427454f7f9a3e49e91d90c416b0e";
+		Optional<App> appOpt =  this.appRepository.findFirstByAppKeyAndAppSecret(wifiAppKey);
 		if(appOpt.isPresent()) {
 			app = appOpt.get();
 		}
