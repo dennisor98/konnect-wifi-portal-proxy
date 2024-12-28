0b1a126281318d958e19945db3da574b82b9a254
Dennis Ronny <dennisor98@gmail.com>
2024-12-28 15:01:51 +0300

    fixed multiple value returns in transaction query

--- src/main/java/net/sasakonnect/wifi_portal/repository/PaymentRepository.java
+++ src/main/java/net/sasakonnect/wifi_portal/repository/PaymentRepository.java
@@ -17,7 +17,7 @@ public interface PaymentRepository extends JpaRepository<Payment, String> {
     @Query("SELECT p FROM Payment p JOIN FETCH p.app JOIN FETCH p.user WHERE p.txtId = :txtId")
     Optional<Payment> findByTxtIdIgnoreCase(@Param("txtId") String txtId);
     
-    @Query("SELECT p FROM Payment p JOIN FETCH p.app WHERE p.mobileNumber = :mobileNumber AND p.verified = false AND DATE(p.createdAt) = CURRENT_DATE ORDER BY p.createdAt DESC")
+    @Query("SELECT p FROM Payment p JOIN FETCH p.app WHERE p.mobileNumber = :mobileNumber AND p.verified = false AND DATE(p.createdAt) = CURRENT_DATE ORDER BY p.createdAt DESC LIMIT 1")
     Optional<Payment> findxByMobileNumber(@Param("mobileNumber") String mobileNumber);
 
 }
