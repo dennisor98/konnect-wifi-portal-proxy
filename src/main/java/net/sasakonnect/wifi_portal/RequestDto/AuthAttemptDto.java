23c021ab0f7c1e267dda6f30385fa58c08bdd44b
Dennis Ronny <dennisor98@gmail.com>
2025-01-16 16:24:56 +0300

    added desirializer for authAttempt

--- src/main/java/net/sasakonnect/wifi_portal/RequestDto/AuthAttemptDto.java
+++ src/main/java/net/sasakonnect/wifi_portal/RequestDto/AuthAttemptDto.java
@@ -1,9 +1,12 @@
 package net.sasakonnect.wifi_portal.RequestDto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AuthAttemptDto {
    String pagetype;   
    String vlan;
