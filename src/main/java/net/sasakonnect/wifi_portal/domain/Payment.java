5129ad00f191dfe983f3270ff0e3118b645adc6d
Dennis Ronny <dennisor98@gmail.com>
2025-01-17 14:54:37 +0300

    added api for tx status check via sdk

--- src/main/java/net/sasakonnect/wifi_portal/domain/Payment.java
+++ src/main/java/net/sasakonnect/wifi_portal/domain/Payment.java
@@ -33,6 +33,9 @@ public class Payment extends BasePortalDomain {
 	    @Column(name = "konnect_checkout_id", length = 255,nullable = false)
 	    private String konnectCheckoutId;
 	    
+	    @Column(nullable = true)
+	    private Integer amount;
+	    
 	    @Column(nullable = false)
 	    private Boolean verified; 
 	    
