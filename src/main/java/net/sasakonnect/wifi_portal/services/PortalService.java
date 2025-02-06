107159b05cf658a5b8fd23677041b0930d125c1c
Dennis Ronny <dennisor98@gmail.com>
2025-02-06 10:36:28 +0300

    modified cors config

--- src/main/java/net/sasakonnect/wifi_portal/services/PortalService.java
+++ src/main/java/net/sasakonnect/wifi_portal/services/PortalService.java
@@ -195,17 +195,6 @@ public class PortalService {
 					return this.packageRepository.save(pkg);
 				} 
 				return null;
-				//				else {
-				//					log.error("exec else");
-				//					var ipg = packageOpt.get();
-				//					ipg.setDescription(d.getDescription());
-				////					ipg.setActive(d.getActive());
-				//////					ipg.setCost(d.getCost());
-				//////					ipg.setName(d.getName());
-				////					ipg.setNoOfUsers(d.getNoOfUsers());
-				////					ipg.setZone(d.getZone());
-				//					return this.packageRepository.save(ipg);
-				//}
 			})
 			.collect(Collectors.toList());
 		});
