2fc84f32ecd53f5339951b844482ddc8f2bc2b39
Dennis Ronny <dennisor98@gmail.com>
2025-04-13 16:27:48 +0300

    disabled rate limiting for tx query API

--- src/main/java/net/sasakonnect/wifi_portal/controllers/PaymentSdkController.java
+++ src/main/java/net/sasakonnect/wifi_portal/controllers/PaymentSdkController.java
@@ -100,7 +100,7 @@ public class PaymentSdkController {
 
 
 	@ApiOperation(value = "Get Transaction status By Id", notes = "This endpoint gets transaction status by Id")
-	@RateLimit(maxRequests = 100000, durationSeconds = 1)
+//	@RateLimit(maxRequests = 100000, durationSeconds = 1)
 	@PostMapping("transaction/status/query")
 	public Object getTxStatusById(@Valid @RequestBody PollTxStatusDto req,
  			@RequestHeader(value = "App-Key") 
